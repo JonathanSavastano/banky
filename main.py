db7f30fdf201d6683a3c1f13f672eb5265b5134b
Jonny <jonathan.savastano1246@gmail.com>
2025-09-08 15:55:23 -0600

    added header row, and initialized category dicts

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
+pd.set_option('display.max_rows', None) # show all rows
+pd.set_option('display.max_colwidth', None) # no column truncation
 df = pd.read_csv('Checking1.csv')
-imp = df.iloc[:, [0, 1, 4]]
-descriptions = imp.iloc[:, 2]
-descriptions_dict = {}
+imp = df.iloc[:, [0, 1, 4]] # Only columns 1, 2, and 5
+imp.columns = ["Date", "Amount", "Description"]
+
+descriptions = imp["Description"] # column 5
+descriptions_dict = {} 
 
 # need to find keywords such as "RECURRING" or "MAVERIK" or "PETCO" that can distinguish what the puchase was
 # make dictionary to hold these categories so we can categorize purchases
@@ -12,4 +16,13 @@ for desc in descriptions:
     key = ' '.join(desc.split()[:2])
     descriptions_dict[key] = desc
 
-print(descriptions_dict)
+print(imp)
+
+
+venmo_payments = {}
+venmo_cashout = {}
+recurring_payments = {}
+purchases = {} 
+online_transfers = {}
+payroll = {}
+student_loan = {}
