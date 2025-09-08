ce9d02ff83cc0504ac7fabf0d0a8149c0a12c051
Jonny <jonathan.savastano1246@gmail.com>
2025-09-08 15:42:56 -0600

    minor changes

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import pandas as pd
 
 df = pd.read_csv('Checking1.csv')
-print(df.iloc[:, [0, 1, 4]].to_string())
+imp = df.iloc[:, [0, 1, 4]]
+descriptions = imp.iloc[:, 2]
+descriptions_dict = {}
+
+# need to find keywords such as "RECURRING" or "MAVERIK" or "PETCO" that can distinguish what the puchase was
+# make dictionary to hold these categories so we can categorize purchases
+
+for desc in descriptions:
+    key = ' '.join(desc.split()[:2])
+    descriptions_dict[key] = desc
+
+print(descriptions_dict)
